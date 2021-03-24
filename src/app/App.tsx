6c00e09b0740d4b4990413af4f51d46ecceef243
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2021-03-24 12:15:25 -0400

    Google Maps

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import { Loader } from "@googlemaps/js-api-loader"
+import React from "react"
 
-import './App.css'
+import "./App.css"
 
 class App extends React.Component
 {
 
+    private loader = new Loader({ apiKey: process.env.REACT_APP_MAPS_KEY!, libraries: ["geometry"] });
+    private map!: google.maps.Map
+
+    private div = React.createRef<HTMLDivElement>()
+
+
+    public async componentDidMount(): Promise<void>
+    {
+        await this.loader.load()
+
+        this.map = new google.maps.Map(this.div.current!, {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        })
+    }
+
     public render(): React.ReactElement
     {
         return (
-            <div>
-                Hello
-            </div>
+            <div id="map" ref={this.div}></div>
         )
     }
 
